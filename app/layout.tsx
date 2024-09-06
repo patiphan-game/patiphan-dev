5c58551df7ca2a1e53c0929c2c83d7f68d5fbc35
Konpwomi <patiphan.arp@gmail.com>
2024-09-06 22:09:02 +0700

    feat: navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "./layout/Navbar";
+import { Quicksand, Poppins } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const quicksand = Quicksand({
+  weight: "300",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = Poppins({
+  weight: "300",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={`${poppins.className}`}>
+      <body className="">
+        <Navbar />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div className="">
-      Home
+    <div className="m-4 mt-16 flex flex-col gap-16">
+      <div className="flex flex-col items-center">
+        <div className="h-[250px] w-[250px] rounded-xl bg-slate-200"></div>
+      </div>
+      <div className="flex w-full flex-col gap-4">
+        <p>Hello world !</p>
+        <h1 className="text-3xl font-medium">I’m Patiphan Arphorn</h1>
+        <p>
+          Im a full stack developer (React.js & Node.js) with a focus on
+          creating (and occasionally designing) exceptional digital experiences
+          that are fast, accessible
+        </p>
+      </div>
+      <div className="h-[2000px]"></div>
     </div>
   );
 }
